f63cab85c4f980c4fdad96a43fe24ac6265e1593
Laurent Laville <farell@php.net>
2008-04-30 20:05:48 +0000

    make it XHTML 1.0 Strict compliant
    git-svn-id: http://svn.php.net/repository/pear/packages/PEAR_PackageUpdate_Web/trunk@258798 c90b9560-bf6c-de11-be94-00142212c4b1

--- Web.php
+++ Web.php
@@ -664,9 +664,9 @@ class PEAR_PackageUpdate_Web extends PEAR_PackageUpdate
         $body = $renderer->toHtml();
 
         $html = <<<HTML
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-    "http://www.w3.org/TR/html4/strict.dtd">
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3c.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
 <title>PEAR_PackageUpdate Web Frontend</title>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
